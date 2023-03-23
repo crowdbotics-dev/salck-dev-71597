0a11f87d02a03605cb1264940a55362cd36ca9b2
Crowdbotics <engineering@crowdbotics.com>
2023-03-23 18:24:09 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/salck_dev_71597/MainActivity.java
@@ -0,0 +1,15 @@
+package com.salck_dev_71597;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "salck_dev_71597";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "salck-dev-71597",
+  url: "https://salck-dev-71597.botics.co",
+  api: "https://salck-dev-71597.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
