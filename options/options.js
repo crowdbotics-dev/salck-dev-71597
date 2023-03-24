668f8c3b0bca7b7be9977ab83de06e5afda3137b
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-03-24 16:49:43 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "salck-dev-71597",
-  url: "https://salck-dev-71597.botics.co",
-  api: "https://salck-dev-71597.botics.co/api/v1"
-}
+  "name": "salck-dev-71597",
+  "url": "https://salck-dev-71597.botics.co",
+  "api": "https://salck-dev-71597.botics.co/api/v1",
+  "initialRoute": "AbouttheAppScreen1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
